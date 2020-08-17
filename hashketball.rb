543c382d405ab458547ef574568d5bd34eba7b5f
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-17 02:53:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,7 @@ def player_numbers(team_name)
     if team[:team_name]==team_name
       team.each do |type,data|
           data.each do |player|
-            jersey_num << player[:number]
+            jersey_num << data[:number]
           end
         end
         
