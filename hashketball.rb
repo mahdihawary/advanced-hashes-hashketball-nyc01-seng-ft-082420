02896304b037e9b52f70506bd314d3d3a9bbd4d1
Mahdi Hawary <mahdihawary@gmail.com>
2020-08-17 02:35:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -145,4 +145,19 @@ def num_points_scored(player_name)
   end
   end
   
-  
+  def shoe_size(player_name)
+  game_hash.each do |key,team|
+    team.each do |type,data|
+      if type == :players
+        
+        data.each do |player|
+          
+          if player[:player_name]==player_name
+            return player[:shoe]
+          end
+        end
+      end
+    end
+  end
+end
+
